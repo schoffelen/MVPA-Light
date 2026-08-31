293f6f0b9501992a0cb955dfc5a0c20193eb41de
treder <matthias.treder@sony.com>
2026-08-31 16:14:07 +0300

    Revert "add helper for selecting samples"
    This reverts commit cb1e69e097445ddad9cfe349022eb865288bca22.

--- utils/mv_select_samples.m
+++
@@ -1,9 +0,0 @@
-function data = mv_select_samples(data, sample_dimension, idx)
-% Index DATA along SAMPLE_DIMENSION(S), keeping only IDX. Generalizes
-% X(idx,:,:,:) style indexing to an arbitrary sample dimension.
-s = repmat({':'}, 1, ndims(data));
-for dim = sort(sample_dimension(:))'
-    s{dim} = idx;
-end
-data = data(s{:});
-end
